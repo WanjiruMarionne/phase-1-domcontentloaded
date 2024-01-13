6e51610cbfd43b017ff1c9c49527aca1f7026764
Wanjiru Mburu <wanjirumarionne@gmail.com>
2024-01-13 13:25:05 +0300

    Completed The JavaScript DOMContentLoaded Event Lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let paragraphContent = document.getElementById("text");
+    paragraphContent.innerHTML = "This is <strong>really</strong> cool!";
+  });
+
+  
